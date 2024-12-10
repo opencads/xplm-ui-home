d2564e4e1acc9002013652c8594287e801167fb1
Cangjier <moodlee@qq.com>
2024-12-10 10:59:38 +0000

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+const env = loadEnv('production', process.cwd());
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_PUBLIC_URL
+  base: `/${env.VITE_PUBLIC_URL}/`
 })
+
+console.log(`VITE_PUBLIC_URL: ${env.VITE_PUBLIC_URL}`);
