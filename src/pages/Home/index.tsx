4ad88a37548f3fa35cd65cc4a16d0c0911820739
Cangjier <moodlee@qq.com>
2024-12-16 09:15:42 +0000

    init

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 import { Flex, useUpdate } from "../../natived";
-import { Button } from "antd";
+import { Button, Spin } from "antd";
 import { SettingOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import { DocumentsApp, IDocumentRecord } from "../../apps/DocumentsApp";
@@ -15,16 +15,18 @@ export interface IHomeProps {
 }
 
 export interface IHomeRef {
-    refresh: () => Promise<void>,
-    archive: () => Promise<void>
+    refresh: (showLoading: boolean) => Promise<void>,
+    archive: (showLoading: boolean) => Promise<void>
 }
 
 export const Home = forwardRef<IHomeRef, IHomeProps>((props, ref) => {
     const [documents, updateDocuments, documentRef] = useUpdate<IDocumentRecord[]>([]);
     const [sidebarVisible, updateSidebarVisible, sidebarVisibleRef] = useUpdate(false);
+    const [loading, updateLoading, loadingRef] = useUpdate(false);
     let navigate = useNavigate();
     const self = useRef<IHomeRef>({
-        refresh: async () => {
+        refresh: async (showLoading: boolean) => {
+            if (showLoading) updateLoading(true);
             try {
                 let documents = await services.getDocumentsFromWorkspace(await services.getDefaultDirectory(), "");
                 updateDocuments(documents.Documents);
@@ -32,20 +34,22 @@ export const Home = forwardRef<IHomeRef, IHomeProps>((props, ref) => {
             catch (e) {
                 console.log(e);
             }
+            if (showLoading) updateLoading(false);
         },
-        archive: async () => {
+        archive: async (showLoading: boolean) => {
+            if (showLoading) updateLoading(true);
             try {
                 let documents = await services.getDocumentsFromWorkspace(await services.getDefaultDirectory(), "");
                 let imports = {
                     Items: []
                 } as IImportInput;
-                for(let document of documents.Documents) {
-                    if(document.local.workspaceState == 'untracked'){
+                for (let document of documents.Documents) {
+                    if (document.local.workspaceState == 'untracked') {
                         imports.Items.push({
                             FilePath: document.local.localFilePath
                         });
                     }
-                    else if(document.local.workspaceState == 'modified') {
+                    else if (document.local.workspaceState == 'modified') {
                         imports.Items.push({
                             FilePath: document.local.localFilePath
                         });
@@ -53,19 +57,21 @@ export const Home = forwardRef<IHomeRef, IHomeProps>((props, ref) => {
                 }
                 await services.importFilesToWorkspace(imports);
             }
-            catch(e) {
+            catch (e) {
 
             }
+            if (showLoading) updateLoading(false);
         }
     });
     useImperativeHandle(ref, () => self.current);
     useEffect(() => {
-        self.current?.refresh();
+        self.current?.refresh(true);
     }, []);
     return <Flex style={{
         ...props.style,
         backgroundColor: '#f4f4f4'
     }} direction='column'>
+        <Spin spinning={loading}></Spin>
         {/* 顶部 */}
         <Flex direction='row' style={{ backgroundColor: '#fff', margin: '0px 0px 2px 0px' }}>
             <Flex style={{ flex: 1 }}>
@@ -74,10 +80,10 @@ export const Home = forwardRef<IHomeRef, IHomeProps>((props, ref) => {
                 }}>{"侧边栏"}</Button>
             </Flex>
             <Flex>
-                <Button type='text' icon={<SettingOutlined onClick={() => {
+                <Button type='text' icon={<SettingOutlined />} onClick={() => {
                     let currentUrl = window.location.pathname;
                     services.openUrl(currentUrl + '/settings');
-                }} />}>{"设置"}</Button>
+                }}>{"设置"}</Button>
             </Flex>
         </Flex>
         {/* 主体 */}
@@ -100,7 +106,7 @@ export const Home = forwardRef<IHomeRef, IHomeProps>((props, ref) => {
                 padding: '4px'
                 // overflowY: 'auto'
             }} direction='column'>
-                <DocumentsApp data={documents} onRefresh={self.current.refresh} onArchive={self.current.archive}></DocumentsApp>
+                <DocumentsApp data={documents} onRefresh={()=>self.current.refresh(true)} onArchive={()=>self.current.archive(true)}></DocumentsApp>
             </Flex>
         </Flex>
     </Flex>
