7c3bb9512584a0277587bbfa241ad43b6fff6606
Cangjier <moodlee@qq.com>
2024-12-26 09:41:53 +0000

    optimize

--- src/apps/WorkspacesApp/index.tsx
+++ src/apps/WorkspacesApp/index.tsx
@@ -37,7 +37,7 @@ export const WorkspacesApp = forwardRef<IWorkspaceAppRef, IWorkspaceAppProps>((p
             render: (value, record) => {
                 return <Flex verticalCenter>
                     <Icon style={{
-                        color: record.active ? 'green' : 'gray'
+                        color: record.active ? 'red' : 'transparent'
                     }} component={ActiveSvg} />
                     <span>{record.name}</span>
                 </Flex>;
@@ -50,7 +50,9 @@ export const WorkspacesApp = forwardRef<IWorkspaceAppRef, IWorkspaceAppProps>((p
             fixed: 'right',
             render: (value, record) => {
                 return <Flex>
-                    <Button icon={<ActiveSvg></ActiveSvg>}>{"Active"}</Button>
+                    <Button icon={<Icon component={ActiveSvg} style={{
+                        color: 'red'
+                    }}></Icon>}>{"Active"}</Button>
                 </Flex>;
             }
         }
